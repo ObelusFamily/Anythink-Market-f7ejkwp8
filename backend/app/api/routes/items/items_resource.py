9b14688564c3724322084e7f8b9556e2faa08562
Ido Grosberg <idogrosberg@gmail.com>
2022-12-15 19:33:05 +0000

    Fix default image on backend

--- backend/app/api/routes/items/items_resource.py
+++ backend/app/api/routes/items/items_resource.py
@@ -68,6 +68,8 @@ async def create_new_item(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=strings.ITEM_ALREADY_EXISTS,
         )
+    img = item_create.image or '/placeholder.png'
+
     item = await items_repo.create_item(
         slug=slug,
         title=item_create.title,
@@ -75,7 +77,7 @@ async def create_new_item(
         body=item_create.body,
         seller=user,
         tags=item_create.tags,
-        image=item_create.image
+        image=img
     )
     send_event('item_created', {'item': item_create.title})
     return ItemInResponse(item=ItemForResponse.from_orm(item))
